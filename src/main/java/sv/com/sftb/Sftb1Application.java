c682d41fa90e7582abd08eba71ef59898f8f504f
Jaquelin Benitez <jackiebenitez20@gmail.com>
2019-04-14 11:07:38 -0600

    ”avance”

---
+++ src/main/java/sv/com/sftb/Sftb1Application.java
@@ -0,0 +1,13 @@
+package sv.com.sftb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sftb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sftb1Application.class, args);
+	}
+
+}
